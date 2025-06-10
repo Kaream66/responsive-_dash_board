f18507165494d9b266b0ad83b1c7e522db79ca6d
Kaream <kareamtanany@gmail.com>
2025-06-10 10:20:54 +0300

    create all expenses widget

--- lib/views/widgets/dashboard_desktop_layout.dart
+++ lib/views/widgets/dashboard_desktop_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/views/widgets/custom_drawer.dart';
 
 class DashboardDesktopLayout
@@ -8,7 +9,18 @@ class DashboardDesktopLayout
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [Expanded(child: CustomDrawer())],
+      children: [
+        Expanded(child: CustomDrawer()),
+        SizedBox(width: 32),
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: [
+              Expanded(child: AllExpenses()),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
