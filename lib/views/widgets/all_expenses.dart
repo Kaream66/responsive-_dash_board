809facd6ee70f39c2a5d8726b349fedbb50fb780
Kaream <kareamtanany@gmail.com>
2025-06-11 15:18:31 +0300

    refactor

--- lib/views/widgets/all_expenses.dart
+++ lib/views/widgets/all_expenses.dart
@@ -2,19 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'package:responsive_dash_board/views/widgets/all_expenses_headers.dart';
 import 'package:responsive_dash_board/views/widgets/all_expenses_item_list_view.dart';
+import 'package:responsive_dash_board/views/widgets/custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
+    return CustomBackgroundContainer(
       child: Column(
         children: [
           AllExpensesHeaders(),
